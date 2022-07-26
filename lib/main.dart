adb5e24b1c10e44aa3662cfaa9d22794e7f2642c
Waratchaya-WL <dada17580@gmail.com>
2022-07-27 00:26:09 +0700

    da12

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int number = 0; // การสร้าง state
 
-  
 // แสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
@@ -34,19 +33,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: ListView(
-          children:getData(10),
+          children: getData(20),
         ),
       ),
     );
   }
 
 // เตรียมข้อมูล
-  List<Widget> getData(int count){
+  List<Widget> getData(int count) {
     // กลุ่มข้อมูล Text Wiget
     List<Widget> data = [];
-  for(var i = 0 ;i<count;i++){
-      data.add(Text("เมนูที่ ${i+1}",style: TextStyle(fontSize: 25),));
+    for (var i = 0; i < count; i++) {
+      var menu = ListTile(
+        title: Text("เมนูที่ ${i + 1}",style:  TextStyle(fontSize: 25),),
+        subtitle: Text("หัวข้อย่อยที่ ${i + 1}"),
+      );
+      data.add(menu);
+    }
+    return data;
   }
-  return data;
- }
 }
