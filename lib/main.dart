98550294f0a200b739a2fdbcf2a88d93f4469fba
Waratchaya-WL <dada17580@gmail.com>
2022-07-26 16:21:37 +0700

    da3

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     title: "my App",
-    home: Text("Hello Dart"),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Flutter ค่ะ"),
+      ),
+      body: Text("สวัสดีค่ะทุกคน"),
+    ),
   );
   runApp(app);
 }
